c6171a65bab42caf8034810a9c8d6063f71e3ef1
mauronofrio <poronofrio@gmail.com>
2020-03-21 16:04:17 +0100

    RMX1901: initial RevengeOS bringup

---
+++ revengeos_RMX1901.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifer: Apache-2.0
+#
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Inherit some common AOSP stuff.
+IS_PHONE := true
+TARGET_DENSITY := xxxhdpi
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Inherit from RMX1901 device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := RMX1901
+PRODUCT_NAME := revengeos_RMX1901
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme X
+PRODUCT_MANUFACTURER := Realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20190718.013112 release-keys"
+
+BUILD_FINGERPRINT := "Realme/RMX1901/RMX1901:9/PKQ1.190101.001/1561987145:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="RMX1901" \
+    TARGET_DEVICE="RMX1901"
